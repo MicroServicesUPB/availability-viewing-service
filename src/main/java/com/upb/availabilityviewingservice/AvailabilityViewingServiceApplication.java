233aab6694af64383212bd12ad38f3ae58722896
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2023-12-14 13:34:07 -0400

    first commit

---
+++ src/main/java/com/upb/availabilityviewingservice/AvailabilityViewingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upb.availabilityviewingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilityViewingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvailabilityViewingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upb/availabilityviewingservice/AvailabilityViewingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upb.availabilityviewingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvailabilityViewingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
